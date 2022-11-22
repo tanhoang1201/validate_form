b656a468d7f514f0a5e043420b6c660ad5f1364b
tanhoang <htan09817@gmail.com>
2022-11-22 19:13:43 +0700

    ok

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,32 +1,86 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
 import InputGroup from "../InputGroup";
 
 function Form() {
-    return (
-        <div className="w-[300px] mx-auto mt-10">
-            <h1 className="text-blue-600 text-xl font-bold text-center mb-5">
-                Sign Up
-            </h1>
-            <form className="flex flex-col gap-[6px]">
-                <InputGroup
-                    placeholder="Enter your username..."
-                    label="Username"
-                    id="username"
-                />
+	const { register, handleSubmit } = useForm();
 
-                <InputGroup
-                    placeholder="Enter your password..."
-                    label="Password"
-                    id="password"
-                />
+	const submitHandle = (values) => {
+		console.log(values);
+	};
 
-                <InputGroup
-                    placeholder="Enter your email address..."
-                    label="Email address"
-                    id="email"
-                />
-            </form>
-        </div>
-    );
+	return (
+		<div className="w-[300px] mx-auto mt-10">
+			<h1 className="text-blue-600 text-xl font-bold text-center mb-5">Sign Up</h1>
+			<form className="flex flex-col gap-4" onSubmit={handleSubmit(submitHandle)}>
+				<InputGroup
+					placeholder="Enter your username..."
+					label="Username"
+					id="username"
+					name="username"
+					register={register}
+				/>
+
+				<InputGroup
+					type="password"
+					placeholder="Enter your password..."
+					label="Password"
+					id="password"
+					name="password"
+					register={register}
+				/>
+
+				<InputGroup
+					placeholder="Enter your email address..."
+					label="Email address"
+					type="email"
+					id="email"
+					name="email"
+					register={register}
+				/>
+
+				<div>
+					<label className="font-semibold">Gender</label>
+					<div className="flex justify-evenly">
+						<div className="flex items-center gap-1">
+							<InputGroup
+								type="radio"
+								name="gender"
+								register={register}
+								className="cursor-pointer"
+								value="male"
+							/>
+							<span>Male</span>
+						</div>
+						<div className="flex items-center gap-1">
+							<InputGroup
+								type="radio"
+								name="gender"
+								register={register}
+								className="cursor-pointer"
+								value="female"
+							/>
+							<span>Female</span>
+						</div>
+					</div>
+				</div>
+
+				<div className="flex items-center gap-2 mt-4">
+					<InputGroup type="checkbox" name="terms" register={register} className="cursor-pointer" />
+					<span className="text-sm">I accept the terms and conditions</span>
+				</div>
+
+				<button
+					type="submit"
+					className="bg-blue-500 text-white font-semibold text-lg rounded-md py-2 mt-4"
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default Form;
--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -1,17 +1,20 @@
-function InputGroup({ label, ...rest }) {
-    return (
-        <div className="flex flex-col gap-[2px]">
-            <label className="font-semibold cursor-pointer" htmlFor={rest.id}>
-                {label}
-            </label>
-            <input
-                type="text"
-                {...rest}
-                className=" outline-none p-3 rounded-md border focus:border-blue-500 transition-colors"
-            />
-            <p className="text-red-500 text-sm">{"error messages"}</p>
-        </div>
-    );
+function InputGroup({ label, register, elementType = "input", error, name, className, ...rest }) {
+	let Com = elementType;
+	return (
+		<div className="flex flex-col gap-[2px]">
+			{label && (
+				<label className="font-semibold cursor-pointer" htmlFor={rest.id}>
+					{label}
+				</label>
+			)}
+			<Com
+				{...rest}
+				{...register(name)}
+				className={`outline-none p-3 rounded-md border focus:border-blue-500 transition-colors ${className}`}
+			></Com>
+			{error && <p className="text-red-500 text-sm">{"error messages"}</p>}
+		</div>
+	);
 }
 
 export default InputGroup;
