fb1a399e29e94c9fa07b45067a767363bb278a0c
tanhoang <htan09817@gmail.com>
2022-11-22 19:13:43 +0700

    ok

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -1,15 +1,30 @@
-function InputGroup({ label, ...rest }) {
+function InputGroup({
+    label,
+    register,
+    elementType = "input",
+    noLabel,
+    noError,
+    ...rest
+}) {
+    let Com = elementType;
     return (
         <div className="flex flex-col gap-[2px]">
-            <label className="font-semibold cursor-pointer" htmlFor={rest.id}>
-                {label}
-            </label>
-            <input
-                type="text"
+            {noLabel ?? (
+                <label
+                    className="font-semibold cursor-pointer"
+                    htmlFor={rest.id}
+                >
+                    {label}
+                </label>
+            )}
+            <Com
                 {...rest}
+                {...register(rest.id)}
                 className=" outline-none p-3 rounded-md border focus:border-blue-500 transition-colors"
-            />
-            <p className="text-red-500 text-sm">{"error messages"}</p>
+            ></Com>
+            {noError ?? (
+                <p className="text-red-500 text-sm">{"error messages"}</p>
+            )}
         </div>
     );
 }
